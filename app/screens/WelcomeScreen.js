4dd0185718f9c5e0f4215c2386e8ddee437d7de2
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-24 16:09:23 -0700

    added StyleSheet tot tthe componenet

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -17,3 +17,30 @@ function WelcomeScreen(props) {
     </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+});
