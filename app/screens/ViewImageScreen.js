b8a0e9b19db5353129505bf185576144e0f9f19a
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-24 16:23:58 -0700

    added StyleSheets and image style

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 function ViewImageScreen(props) {
-    return (
-        <Image source={require('../assets/chair.jpg')}/>
-    );
+  return <Image style={styles.image} source={require("../assets/chair.jpg")} />;
 }
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default ViewImageScreen;
