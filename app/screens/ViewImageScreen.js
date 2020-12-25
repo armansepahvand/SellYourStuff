ea94133f1195053da3031d13de7f94b208f1aa85
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-24 20:36:07 -0700

    Added two place holders for buttons

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -3,16 +3,38 @@ import { Image, StyleSheet, Text, View } from "react-native";
 
 function ViewImageScreen(props) {
   return (
-      <View>
-          <Image
-            resizeMode="contain"
-            style={styles.image}
-            source={require("../assets/chair.jpg")}
-          />
-      </View>
+    <View style={styles.container}>
+      <View style={styles.CloseIcon}></View>
+      <View style={styles.DeleteIcon}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
+    </View>
   );
 }
+
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+  },
+  CloseIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fc5c65",
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  DeleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#4ecdc4",
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
   image: {
     width: "100%",
     height: "100%",
