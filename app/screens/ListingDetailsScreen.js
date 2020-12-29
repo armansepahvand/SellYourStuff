dc4b5c8a2218d6a0826f43ff700c14925b4b637a
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-28 22:14:18 -0700

    Added ListItem component wrapped in a container

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Image, View, StyleSheet } from "react-native";
 import AppText from "../components/AppText";
 import colors from "../config/colors";
+import ListItem from "../components/ListItem";
 
 function ListingDetailsScreen(props) {
   return (
@@ -10,6 +11,13 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Green jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/Arman.png")}
+            title="Arman Sepah Vand"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
