2c4bf4231e0fc2ed152c2754b1093f8eb5c5feb7
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-28 20:29:56 -0700

    Added ListingDetailsScreen

---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image, View,StyleSheet } from 'react-native';
+
+function ListingDetailsScreen(props) {
+    return (
+       
+    );
+}
+
+const styles = StyleSheet.create({
+   
+    
+})
+
+export default ListingDetailsScreen;
