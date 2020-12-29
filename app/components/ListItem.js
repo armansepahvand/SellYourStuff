c8657921ce3a66ff73ae79539450bb2ebb77922f
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-28 21:13:28 -0700

    Added ListItem component

---
+++ app/components/ListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View,StyleSheet } from 'react-native';
+
+function ListItem({image,title,subTitle}) {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default ListItem;
