52efb65bebf41cc13f1dd830f46484384d5777b5
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-28 20:43:03 -0700

    added style for the text components

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Image, View, StyleSheet } from "react-native";
 import AppText from "../components/AppText";
+import colors from "../config/colors";
 
 function ListingDetailsScreen(props) {
   return (
     <View>
       <Image style={styles.image} source={require("../assets/jacket.jpg")} />
       <View style={styles.detailsContainer}>
-        <AppText>Green jacket for sale</AppText>
-        <AppText>$100</AppText>
+        <AppText style={styles.title}>Green jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
       </View>
     </View>
   );
@@ -21,6 +22,16 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: 300,
+  },
+  price: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: colors.secondary,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
   },
 });
 
