bc50bc0367434882a2712f51f03b82f1ad2c1788
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-28 20:36:45 -0700

    added to text components wraped in a container

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Image, View, StyleSheet } from "react-native";
+import AppText from "../components/AppText";
 
 function ListingDetailsScreen(props) {
   return (
     <View>
       <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <View style={styles.detailsContainer}>
+        <AppText>Green jacket for sale</AppText>
+        <AppText>$100</AppText>
+      </View>
     </View>
   );
 }
