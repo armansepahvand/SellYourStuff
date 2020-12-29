494d81ddd1d507414c13b2e809435298300eeb01
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-28 22:19:41 -0700

    Added image and text componenets to the ListItem component

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { View,StyleSheet } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import AppText from "../components/AppText";
+import colors from "../config/colors";
 
-function ListItem({image,title,subTitle}) {
+function ListItem({ image, title, subTitle }) {
     return (
-        <View>
-
+    <View style={styles.container}>
+      <Image style={styles.image} source={image}></Image>
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
         </View>
     );
 }
