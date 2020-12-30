85a0efbd3ba33370f2e832aa46955e5195308178
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-30 16:51:33 -0700

    Created ListingScreen

---
+++ app/screens/ListingsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import Screen from '../components/Screen';
+
+function ListingsScreen(props) {
+    return (
+        <Screen>
+            <FlatList/>
+        </Screen>
+    );
+}
+
+export default ListingsScreen;
