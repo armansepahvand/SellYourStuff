b08942643fd98b5151360ae89e2cb83c8f0866dc
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-30 16:40:30 -0700

    Created Acount Screen

---
+++ app/screens/AccountScreen.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ListItem from "../components/ListItem";
+import Screen from "../components/Screen";
+import { StyleSheet, View, FlatList } from "react-native";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
+import ListItemSeperator from "../components/ListItemSeperator";
+
+const menuItems = [
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
+
+function AccountScreen(props) {
+  return (
+  );
+}
+
+
+
+export default AccountScreen;
