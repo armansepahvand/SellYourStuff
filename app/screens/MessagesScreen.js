9fb0e43c90081f7cf7be94f5c2e47bcdcc80a46f
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-29 21:18:19 -0700

    added a test object for our screen

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,4 +1,19 @@
-import React from 'react';
+import { FlatList } from "react-native";
+import ListItem from "../components/ListItem";
+const messages = [
+  {
+    id: 1,
+    title: "T1",
+    description: "D1",
+    image: require("../assets/arman.png"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/arman.png"),
+  },
+];
 
 function MessagesScreen(props) {
     return (
