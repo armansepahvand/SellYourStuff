021f0ac9e15f83f351ea3cc802f00172e02cde86
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-30 00:32:55 -0700

    added flex:1 to screen style

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import constants from "expo-constants";
+import colors from "../config/colors";
 
 function Screen({ children }) {
   return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
@@ -9,6 +10,7 @@ function Screen({ children }) {
 const styles = StyleSheet.create({
   screen: {
     paddingTop: constants.statusBarHeight,
+    flex: 1,
   },
 });
 
