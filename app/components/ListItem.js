d93f17179acca8d9462e76195f14c3c15b50255a
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-29 22:46:34 -0700

    added touch feedback to the component with a onPress prop

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
 import AppText from "../components/AppText";
 import colors from "../config/colors";
 
-function ListItem({ image, title, subTitle }) {
+function ListItem({ image, title, subTitle, onPress }) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image}></Image>
-      <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
+    <TouchableHighlight underlayColor={colors.medium} onPress={onPress}>
+      <View style={styles.container}>
+        <Image style={styles.image} source={image}></Image>
+        <View style={styles.detailsContainer}>
+          <AppText style={styles.title}>{title}</AppText>
+          <AppText style={styles.subTitle}>{subTitle}</AppText>
+        </View>
       </View>
-    </View>
+    </TouchableHighlight>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    padding: 15,
   },
   image: {
     width: 70,
