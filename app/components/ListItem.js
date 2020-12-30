cfce6d778abfc0e490f6e328040d33627350c841
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-29 23:35:41 -0700

    added swipe functionality to listItems

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -2,18 +2,21 @@ import React from "react";
 import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
 import AppText from "../components/AppText";
 import colors from "../config/colors";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
-function ListItem({ image, title, subTitle, onPress }) {
+function ListItem({ image, title, subTitle, onPress, renderRightActions}) {
   return (
-    <TouchableHighlight underlayColor={colors.medium} onPress={onPress}>
-      <View style={styles.container}>
-        <Image style={styles.image} source={image}></Image>
-        <View style={styles.detailsContainer}>
-          <AppText style={styles.title}>{title}</AppText>
-          <AppText style={styles.subTitle}>{subTitle}</AppText>
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight underlayColor={colors.medium} onPress={onPress}>
+        <View style={styles.container}>
+          <Image style={styles.image} source={image}></Image>
+          <View style={styles.detailsContainer}>
+            <AppText style={styles.title}>{title}</AppText>
+            <AppText style={styles.subTitle}>{subTitle}</AppText>
+          </View>
         </View>
-      </View>
-    </TouchableHighlight>
+      </TouchableHighlight>
+    </Swipeable>
   );
 }
 
