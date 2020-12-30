1cab399e5c562f9437adca2084e40fce9b6a6890
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-30 16:57:11 -0700

    Added Listing array as data source

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { FlatList } from 'react-native';
-import Screen from '../components/Screen';
+import React from "react";
+import { FlatList } from "react-native";
+import Screen from "../components/Screen";
+
+const listings = [
+  {
+    id: 1,
+    title: "Green jacket for sale",
+    price: 100,
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Bicycle for sale",
+    price: 200,
+    image: require("../assets/jacket.jpg"),
+  },
+];
 
 function ListingsScreen(props) {
-    return (
-        <Screen>
-            <FlatList/>
-        </Screen>
-    );
+  return (
+    <Screen>
+      <FlatList />
+    </Screen>
+  );
 }
 
 export default ListingsScreen;
