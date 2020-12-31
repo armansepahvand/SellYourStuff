bddf964b9e85ea2ff872e24cbd26a75c678af466
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-30 17:12:48 -0700

    Added Styling

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import AppCard from "../components/AppCard";
 import Screen from "../components/Screen";
+import colors from "../config/colors";
 
 const listings = [
   {
@@ -20,7 +21,7 @@ const listings = [
 
 function ListingsScreen(props) {
   return (
-    <Screen>
+    <Screen style={styles.screen}>
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
@@ -35,5 +36,12 @@ function ListingsScreen(props) {
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 10,
+    backgroundColor: colors.light,
+  },
+});
 
 export default ListingsScreen;
