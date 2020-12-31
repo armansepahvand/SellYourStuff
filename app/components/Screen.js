9bf2aa7a969749d4f8833367dd9b674864bb14e8
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-30 17:12:16 -0700

    Wraapped children object inside a View to enable horizontal padding

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import constants from "expo-constants";
 import colors from "../config/colors";
 
 function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={ style}>{children}</View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
