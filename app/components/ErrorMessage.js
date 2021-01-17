bca90bd22865dfc8acdb2b76ff151a2558988621
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-16 19:47:19 -0700

    addeda AppText tag with the error message and added some styling

--- app/components/ErrorMessage.js
+++ app/components/ErrorMessage.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import AppText from './AppText';
 
-function ErrorMessage(props) {
-  return <div></div>;
+function ErrorMessage({ error }) {
+  if (!error) return null;
+  return <AppText style={styles.error}>{error}</AppText>;
 }
+
+const styles = StyleSheet.create({
+  error: {
+    color: 'red',
+  },
+});
 
 export default ErrorMessage;
