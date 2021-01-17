b542fb58018c5de2a2eb00c4ed992933b745e961
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-17 12:15:18 -0700

    call onRemoveImage prop function from on ChangeImage attribute

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import ImageInput from './ImageInput'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ImageInput from './ImageInput';
 
-export default function ImageInputList({imageUris=[]}) {
-    return (
-        <View style={styles.container}>
-            {imageUris.map(uri=><ImageInput imageUri={uri} onChangeImage={}/> )}
-            
-        </View>
-    )
+export default function ImageInputList({
+  imageUris = [],
+  onRemoveImage,
+  onAddImage,
+}) {
+  return (
+    <View style={styles.container}>
+      {imageUris.map((uri) => (
+        <ImageInput
+          imageUri={uri}
+          key={uri}
+          onChangeImage={() => onRemoveImage(uri)}
+        />
+      ))}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection:"row"
-    }
-})
+  container: {
+    flexDirection: 'row',
+  },
+});
