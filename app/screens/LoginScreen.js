03eb165d773b5e53112369a8c11694c57ac16e3a
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-16 18:00:40 -0700

    Added Logo with styling

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
+import Screen from '../components/Screen';
 
 function LoginScreen(props) {
-  return <div></div>;
+  return (
+    <Screen>
+      <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+    </Screen>
+  );
 }
 
+const styles = StyleSheet.create({
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
 export default LoginScreen;
