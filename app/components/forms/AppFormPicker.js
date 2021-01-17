7589fc4ab4f3c00bb2efe93ae091d07192501766
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-16 22:14:56 -0700

    Created a custome reusable picker for the forms

---
+++ app/components/forms/AppFormPicker.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useFormikContext } from 'formik';
+
+import AppTextInput from '../AppTextInput';
+import ErrorMessage from './ErrorMessage';
+import AppPicker from '../AppPicker';
+
+function AppFormPicker({items, name,placeholder}) {
+    const { setFieldValue, errors, touched, values } = useFormikContext();
+  return (
+    <>
+      <AppPicker
+       items={items}
+       onSelectItem={item=>setFieldValue(name,item)}
+       placeholder={placeholder}
+       selectedItem={values[name]}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+}
+
+export default AppFormPicker;
