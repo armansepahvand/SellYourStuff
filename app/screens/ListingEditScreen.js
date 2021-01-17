f07f487c2d1c98748edbffba7477ff1ac8136f23
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-16 21:56:18 -0700

    Added the Yup validation schema

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -10,6 +10,13 @@ import {
 } from '../components/forms';
 import Screen from '../components/Screen';
 
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().min(1).label('Title'),
+  price: Yup.number().required().min(1).max(10000).label('Price'),
+  description: Yup.string().label('Description'),
+  category: Yup.object().required().nullable().label('Category'),
+});
+
 function ListingEditScreen(props) {
   return <div></div>;
 }
