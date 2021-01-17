75c33a55a883fa9f341281d12747068f7daf7670
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-16 19:44:44 -0700

    Created the ErrorMessage Component

---
+++ app/components/ErrorMessage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import AppText from './AppText';
+
+function ErrorMessage(props) {
+  return <div></div>;
+}
+
+export default ErrorMessage;
