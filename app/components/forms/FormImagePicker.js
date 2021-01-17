2929aa42e0c91c91c743d6bc791aa74bcfcc81de
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-17 16:07:45 -0700

    used formik state to get the forem values and methods to manage the form and saved the form values into the imageUris constant

--- app/components/forms/FormImagePicker.js
+++ app/components/forms/FormImagePicker.js
@@ -2,6 +2,12 @@ import { ErrorMessage } from 'formik';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ImageInputList from '../ImageInputList';
+import useFormikContext from 'formik';
+
+export default function FormImagePicker({ name }) {
+  const { setFieldValue, errors, touched, values } = useFormikContext();
+
+  const imageUris = values[name];
 
 export default function FormImagePicker() {
   return (
