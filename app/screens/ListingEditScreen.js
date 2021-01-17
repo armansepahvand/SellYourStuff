00772ae51bf4188da319b7da18de50be39dbe216
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-16 21:52:15 -0700

    Created the framework for ListingEdit screen

---
+++ app/screens/ListingEditScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import * as Yup from 'yup';
+
+import {
+  AppForm,
+  AppFormField,
+  AppFormPicker,
+  SubmitButton,
+} from '../components/forms';
+import Screen from '../components/Screen';
+
+function ListingEditScreen(props) {
+  return <div></div>;
+}
+
+export default ListingEditScreen;
