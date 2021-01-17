f807c45ed130a640156d3393c5eb736fc285836a
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-16 20:46:05 -0700

    added props for the formik form component and used then in the AppForm function

--- app/components/AppForm.js
+++ app/components/AppForm.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Children } from 'react';
+import Formik from 'formik';
 
-function AppForm(props) {
-    return (
-        <Formik
-        initialValues={{ email: '', password: '' }}
-        onSubmit={(values) => console.log(values)}
-        validationSchema={validationSchema}
-      >
-          </Formik>
-    );
+function AppForm({ initialValues, onSubmit, validationSchema, children }) {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {/* Get the form JSX from the children prop coming from the parent component */}
+      {() => <>{children}</>}
+    </Formik>
+  );
 }
 
 export default AppForm;
