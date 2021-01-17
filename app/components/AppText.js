6e02e64b8a23e999aa1c527d84e764cfd1a4a737
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-17 12:04:15 -0700

    Added otherProps to AppText component

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,10 +1,14 @@
-import React from "react";
-import { Text } from "react-native";
-import defaultStyles from "../config/styles";
+import React from 'react';
+import { Text } from 'react-native';
+import defaultStyles from '../config/styles';
 
 //AppText componenet to create custome Text items by getting the wrapped variable  and style as  props
-function AppText({ children, style }) {
-  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
+function AppText({ children, style, ...otherProps }) {
+  return (
+    <Text style={[defaultStyles.text, style]} {...otherProps}>
+      {children}
+    </Text>
+  );
 }
 
 export default AppText;
