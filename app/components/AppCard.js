e310f648d8c140b84f00f177487c43291aec23d8
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-17 21:37:41 -0700

    refactored the code to make the card dynamic

--- app/components/AppCard.js
+++ app/components/AppCard.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { View, StyleSheet, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import colors from '../config/colors';
 import AppText from './AppText';
 
 // AppCard componenet to create custome cards by getting the title, subtitle and image as props from parent componenet
-function AppCard({ title, subTitle, image }) {
+function AppCard({ title, subTitle, image, onPress }) {
   return (
-    <View style={styles.card}>
-      <Image source={image} style={styles.image} />
-      <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.card}>
+        <Image source={image} style={styles.image} />
+        <View style={styles.detailsContainer}>
+          <AppText style={styles.title}>{title}</AppText>
+          <AppText style={styles.subTitle}>{subTitle}</AppText>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
