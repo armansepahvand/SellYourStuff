88bd2a62370333796673409541ca6906f3017980
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-17 20:09:49 -0700

    export JSX component

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
@@ -11,5 +13,6 @@ const AuthNavigator = () => (
     <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="Register" component={RegisterScreen} />
   </Stack.Navigator>
-
 );
+
+export default AuthNavigator;
