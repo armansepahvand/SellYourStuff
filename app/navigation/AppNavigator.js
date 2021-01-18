1654b5a8907cb506be8015697e287cc312b57f75
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-17 21:19:00 -0700

    Added Three tabs to the navigator

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,7 +1,18 @@
-import {  createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Tab =createBottomTabNavigator();
+import AccountScreen from '../screens/AccountScreen';
+import ListingEditScreen from '../screens/ListingEditScreen';
+import ListingsScreen from '../screens/ListingsScreen';
 
-const AppNavigator= ()=>(
-    
-)
+const Tab = createBottomTabNavigator();
+
+const AppNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Listings" component={ListingsScreen} />
+    <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
+    <Tab.Screen name="Account" component={AccountScreen} />
+  </Tab.Navigator>
+);
+
+export default AppNavigator;
