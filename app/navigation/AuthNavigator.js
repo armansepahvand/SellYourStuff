002b582b3071059814f73790b21cdfb823564f01
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-17 20:08:23 -0700

    Added Stack Screens

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -7,7 +7,9 @@ const Stack = createStackNavigator();
 
 const AuthNavigator = () => (
   <Stack.Navigator>
-    
+    <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
   </Stack.Navigator>
 
 );
