62112b256b0b0e6c24ef89852e9661144f3c62cd
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-17 20:07:59 -0700

    Created the navigator for  for Welcome screen, Login screen and Register screen

---
+++ app/navigation/AuthNavigator.js
@@ -0,0 +1,13 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
+
+const Stack = createStackNavigator();
+
+const AuthNavigator = () => (
+  <Stack.Navigator>
+    
+  </Stack.Navigator>
+
+);
