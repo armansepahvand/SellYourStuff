6d45cfc99ccf0af38732ef9cd212cd9404a3707a
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-21 21:34:16 -0700

    fixed the issue with color pallet by specifying black for the color attribute

--- app/config/styles.js
+++ app/config/styles.js
@@ -5,7 +5,7 @@ import colors from './colors';
 export default {
   colors,
   text: {
-    color: colors,
+    color: colors.black,
     fontSize: 18,
     fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
   },
