768392463bd59b91ab3399a89312692276fa6569
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-27 09:36:06 -0700

    added the post method to  post listings to the server

--- app/api/listings.js
+++ app/api/listings.js
@@ -6,6 +6,32 @@ const endpoint = '/listings';
 //function to get all the listings from server
 const getlistings = () => client.get(endpoint);
 
+//function to post a listing to the server
+const addListing = (listing) => {
+  //create a new formData object to store the listing object in the form type
+  const data = new FormData();
+
+  data.append('title', listing.title);
+  data.append('price', listing.price);
+  data.append('categoryId', listing.category.value);
+  data.append('description', listing.description);
+
+  listing.images.forEach((image, index) =>
+    data.append('images', {
+      name: 'image' + index,
+      type: 'image/jpeg',
+      uri: image,
+    })
+  );
+
+  //if the listting has a location associated to it set add it to the data
+  if (listing.location)
+    data.append('location', JSON.stringify(listing.location));
+
+  return client.post(endpoint, data);
+};
+
 export default {
+  addListing,
   getlistings,
 };
